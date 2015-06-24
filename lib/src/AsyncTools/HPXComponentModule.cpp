1341749cd1a3328cae668615c6bf5908f528a45b
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2015-06-24 01:02:33 +0000

    First-pass fixes for DistributedAsyncLevelData, add DistributedAsyncLevelData component registration code.
    git-svn-id: https://anag-repo.lbl.gov/svn/IntelCAM/trunk@204 3f8307fa-4a67-4cca-8a16-6d3bb92dd138

--- lib/src/AsyncTools/HPXComponentModule.cpp
+++ lib/src/AsyncTools/HPXComponentModule.cpp
@@ -16,18 +16,53 @@
 #include <boost/serialization/export.hpp>
 
 #include "HPXServices.H"
+#include "DistributedAsyncLevelData.H"
 
-#ifdef CH_NAMESPACE
-    #define CH_XD Chombo
-#else
-    #define CH_XD
-#endif
+#include "NamespaceVar.H"
 
 HPX_REGISTER_COMPONENT_MODULE();
 
 ///////////////////////////////////////////////////////////////////////////////
+
 HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(
     hpx::components::simple_component<CH_XD::HPXServicesServer>,
-    chombo_services_server);
+    chombo_ServicesServer);
 
+///////////////////////////////////////////////////////////////////////////////
+
+// Add function-style overloading for the registeration macro using a forwarding
+// variadic macro.
+#define HPX_CHOMBO_REGISTER_ALD_ACTIONS(...)                                \
+        HPX_CHOMBO_REGISTER_ALD_(__VA_ARGS__)                               \
+    /**/
+
+#define HPX_CHOMBO_REGISTER_ALD_(...)                                       \
+    HPX_UTIL_EXPAND_(BOOST_PP_CAT(                                          \
+        HPX_CHOMBO_REGISTER_ALD_,                                           \
+            HPX_UTIL_PP_NARG(__VA_ARGS__)                                   \
+    )(__VA_ARGS__))                                                         \
+    /**/
+
+// One argument overload, uses the typename as the registeration name.
+#define HPX_CHOMBO_REGISTER_ALD_1(T)                                        \
+    HPX_CHOMBO_REGISTER_ALD_2(T, T)                                         \
+    /**/
+
+// Two argument overload, uses a user-specified name for registeration. This
+// version must be used if the type T is not alphanumeric (e.g. if it's a
+// template, or namespace qualified). 
+#define HPX_CHOMBO_REGISTER_ALD_2(T, name)                                  \
+    HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(                                 \
+        hpx::components::managed_component<                                 \
+            CH_XD::AsyncLevelDataServer<T>                                  \
+        >,                                                                  \
+        chombo_AsyncLevelDataServer_ ## name                                \
+    );                                                                      \
+    HPX_REGISTER_ACTION(                                                    \
+        CH_XD::AsyncLevelDataServer<T>::applyElements_action,               \
+        chombo_AsyncLevelDataServer_applyElements_ ## name                  \
+    );                                                                      \
+    /**/
+
+HPX_CHOMBO_REGISTER_ALD_ACTIONS(CH_XD::FArrayBox, FArrayBox); 
 
